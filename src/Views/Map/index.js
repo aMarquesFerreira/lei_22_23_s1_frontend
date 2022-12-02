611306b3fb70a7e608861b937947370814c29588
HugoRodrigu <1220252@isep.ipp.pt>
2022-12-02 15:08:44 +0000

    style map correction

--- src/Views/Map/index.js
+++ src/Views/Map/index.js
@@ -8,14 +8,14 @@ import { OrbitControls } from 'three-stdlib';
 
 export default function Map() {
   return (
-    <div className="section-map-svg">
-      <Canvas
-        frameloop="demand"
-        orthographic
-        camera={{ position: [0, 0, 50], zoom: 2, far: 10000 }}>
-        <fog attach="fog" args={['white', 0, 500]} />
-        <Scene />
-      </Canvas>
-    </div>
+    <Canvas
+      style={{height: "300px"}}
+      className="section-map-svg"
+      frameloop="demand"
+      orthographic
+      camera={{ position: [0, 0, 50], zoom: 2, far: 10000 }}>
+      <fog attach="fog" args={['white', 0, 500]} />
+      <Scene />
+    </Canvas>
   );
-} 
+}
